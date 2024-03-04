eb47da896dff066b85ea302c1fe94f504ed96f00
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-03-04 12:24:58 +0100

    Step 18

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const numberInput = document.getElementById('number-input');
 const convertBtn = document.getElementById('convert-btn');
 const result = document.getElementById('result');
 
-function decimalToBinary(input) {};
+function decimalToBinary(input) {
+    return '1';
+};
 
 function checkUserInput() {
     if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
@@ -21,3 +23,11 @@ numberInput.addEventListener('keydown', (e) => {
         checkUserInput();
     }
 });
+
+/*
+    NOTES:
+
+    - Binary numbers are a base-2 number system. Unlike the base-10 or decimal number system we use every day that uses 10 digits (0-9) to form numbers, the binary number system only has two digits, 0 and 1. 
+    In computer science, these binary digits are called bits, and are the smallest unit of data computers can process. 
+    For computers, 0 represents false or "off", and 1 represents true or "on".
+*/
